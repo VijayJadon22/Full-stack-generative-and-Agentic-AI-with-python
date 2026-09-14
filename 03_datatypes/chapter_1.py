c59e9fb9d241327bd766daa653666319046effd6
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2026-09-14 15:44:47 +0530

    Refactor variable assignment formatting and enhance chai description output

--- 03_datatypes/chapter_1.py
+++ 03_datatypes/chapter_1.py
@@ -1,9 +1,15 @@
-item_count=3
+item_count = 3
 print(f"this is the {item_count} item count")
 
-item_count=8
+item_count = 8
 print(f"this is the {item_count} item count")
 
 print(f"Id of 3: {id(3)}")
 print(f"Id of 8: {id(8)}")
 
+chai_description = "This is a chai description"
+print(
+    f"{chai_description.split(' ')[0]} {chai_description.split(' ')[1]} {chai_description.split(' ')[3]}"
+)
+print(chai_description.index("ai"))
+print(f"{chai_description[0:7]} {chai_description[10:14]}")
