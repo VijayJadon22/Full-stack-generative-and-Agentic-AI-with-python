2e27af1ada2d9b5cf5f03c917be9617a99330961
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2026-09-14 11:46:38 +0530

    Add test.py to demonstrate variable assignment and id function usage

---
+++ 03_datatypes/test.py
@@ -0,0 +1,9 @@
+item_count=3
+print(f"this is the {item_count} item count")
+
+item_count=8
+print(f"this is the {item_count} item count")
+
+print(f"Id of 3: {id(3)}")
+print(f"Id of 8: {id(8)}")
+
