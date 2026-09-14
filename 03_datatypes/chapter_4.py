ebbfa097dd9c9b0db7c66c4fbe8a50d5c32c73ae
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2026-09-14 20:18:38 +0530

    Add chapter_4.py to demonstrate list operations including append and extend

---
+++ 03_datatypes/chapter_4.py
@@ -0,0 +1,10 @@
+list_items=["Pant","shirt","socks"]
+list_items2=["Cargo"]
+
+print(list_items)
+
+list_items.append("joggers")
+print(list_items)
+print(len(list_items))
+list_items.extend(list_items2)
+print(list_items)
