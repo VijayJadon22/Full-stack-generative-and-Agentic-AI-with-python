057baa8170f7383b1b372509b7a8433feb0fd262
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2026-09-15 15:44:16 +0530

    Refactor walrus_operator.py to comment out unused code examples and improve clarity

--- 05_loops/walrus_operator.py
+++ 05_loops/walrus_operator.py
@@ -1,4 +1,20 @@
-value=13
+# value = 13
 
-if (remainder:=value%5):
-    print(f"Not divisible, remainder is {remainder}")
+# if remainder := value % 5:
+#     print(f"Not divisible, remainder is {remainder}")
+
+# available_sizes = ["small", "medium", "large"]
+
+# if (requested_size := input("Enter the desired size: ")) in available_sizes:
+#     print(f"Serving {requested_size} chai")
+# else:
+#     print(f"Size is unavailable - {requested_size}")
+
+flavors=["ginger","masala","lemon","mint"]
+
+print("Available flavors ",flavors)
+
+while (flavor := input("Choose the flavor: ")) not in flavors:
+    print(f"Sorry {flavor} is not available")
+print(f"You choose {flavor} chai")
+
