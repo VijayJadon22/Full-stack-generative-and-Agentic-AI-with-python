d6af8145f68248d439a85d64dadc0b99bb3d7162
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2026-09-15 13:13:58 +0530

    Add index.py and test2.py to display user orders and menu items

---
+++ 05_loops/index.py
@@ -0,0 +1,4 @@
+user_list=["Aman","Nishant","Rohan"]
+
+for user in user_list:
+    print(f"Order is ready for: {user}")
---
+++ 05_loops/test2.py
@@ -0,0 +1,4 @@
+menu = ["Green", "Lemon", "Spiced", "Mint", "Ginger"]
+
+for idx, item in enumerate(menu, start=1):
+    print(f"Menu items is {idx}:{item}")
