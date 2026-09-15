f285cfa8e3c8048b295fcf5e2c18f4debf189778
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2026-09-15 12:13:28 +0530

    Add index.py and test.py to demonstrate conditional statements and user input handling

---
+++ 04_conditionals/index.py
@@ -0,0 +1,6 @@
+kettle_boiled=True
+
+if kettle_boiled:
+    print("Kettle boiled!")
+else:
+    print("Please boil the water in kettle")
---
+++ 04_conditionals/test.py
@@ -0,0 +1,10 @@
+items=["socks","pant","shirt"]
+user_input = input("Enter your preferred Item: ")
+
+if user_input in items:
+    print(f"{user_input} is available in items")
+else:
+    print(f"{user_input} is not available in items")
+
+
+
