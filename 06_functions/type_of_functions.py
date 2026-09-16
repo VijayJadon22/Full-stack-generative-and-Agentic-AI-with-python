0a217cf41e429831936582e9eb8f3fef888baa99
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2026-09-16 20:05:27 +0530

    Add recursive_function to demonstrate recursion in type_of_functions.py

--- 06_functions/type_of_functions.py
+++ 06_functions/type_of_functions.py
@@ -18,3 +18,14 @@ new_bill = calculate_total(100)
 print(f"New bill is: {new_bill}")
 
 print(f"Inital bill: {initial_bill}")
+
+
+# Recursive Function
+
+def recursive_function(n):
+    if(n==0):
+        return "N value is 0"
+    print(f"Value of n is: {n}")
+    return recursive_function(n-1)
+
+recursive_function(5)
