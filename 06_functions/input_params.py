fc64533ab31115bc1f9f8ce417a9b5564220907f
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2026-09-16 19:55:19 +0530

    Add special_chai function to handle variable ingredients and extras

---
+++ 06_functions/input_params.py
@@ -0,0 +1,6 @@
+def special_chai(*ingredients, **extras):
+    print(f"Ingredients are: {ingredients}")
+    print(f"Extras are: {extras}")
+
+
+special_chai("Ginger", "ELaichi", sweetner="Narural", quantity=2)
