64051164d7a249dcea473b90671ce0eac642e818
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2026-09-16 20:47:22 +0530

    Refactor main.py to clean up imports and streamline function calls for returning t-shirts and pants

--- 07_item_business/main.py
+++ 07_item_business/main.py
@@ -1,4 +1,10 @@
-import categories.items
+# import categories.items
 
-print(categories.items.return_tshirts())
-print(categories.items.return_pants())
+# print(categories.items.return_tshirts())
+# print(categories.items.return_pants())
+
+
+from categories.items import return_tshirts as tshirts, return_pants as pants
+
+print(tshirts())
+print(pants())
