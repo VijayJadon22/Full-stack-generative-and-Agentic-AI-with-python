764f20d4111b8959abfef4bb7fee00c7e232cd9e
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2026-09-16 20:42:00 +0530

    Add items module with functions to return t-shirts and pants

---
+++ 07_item_business/categories/items.py
@@ -0,0 +1,6 @@
+def return_tshirts():
+    return "This func return tshirts"
+
+
+def return_pants():
+    return "This func return pants"
---
+++ 07_item_business/main.py
@@ -0,0 +1,4 @@
+import categories.items
+
+print(categories.items.return_tshirts())
+print(categories.items.return_pants())
