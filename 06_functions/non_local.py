fe490df46a7a3362b697718da7634eeb248b954d
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2026-09-16 16:58:11 +0530

    Add non_local.py to demonstrate variable scope in nested functions

---
+++ 06_functions/non_local.py
@@ -0,0 +1,12 @@
+def update_item_order():
+    item_category = "Tshirts"
+
+    def inner_func():
+        item_category = "Shirts"
+        print(f"Inner Item category is {item_category}")
+
+    inner_func()
+    print(f"Outer Item category is {item_category}")
+
+
+update_item_order()
