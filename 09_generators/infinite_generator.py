05fe580f7bf88f270d733c74a6e437ca5cc588a4
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2026-09-17 22:14:41 +0530

    Add infinite_chai generator to yield an infinite sequence of counts

---
+++ 09_generators/infinite_generator.py
@@ -0,0 +1,16 @@
+def infinite_chai():
+    count = 1
+    while True:
+        yield f"Count is {count}"
+        count += 1
+
+
+refill = infinite_chai()
+
+user2 = infinite_chai()
+
+for _ in range(3):
+    print(next(refill))
+
+for _ in range(5):
+    print(next(user2))
