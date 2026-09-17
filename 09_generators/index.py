297d85c5adb57042cdf69767be10b32b9b9f6f65
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2026-09-17 20:25:32 +0530

    Add chai list and generator functions to index.py

--- 09_generators/index.py
+++ 09_generators/index.py
@@ -6,5 +6,23 @@ def serve_chai():
 
 stall = serve_chai()
 
-for cup in stall:
-    print(cup)
+# for cup in stall:
+#     print(cup)
+
+
+# normal function
+def get_chai_list():
+    return ["cup1", "cup2", "cup3"]
+
+
+# Generator function
+def get_chai_gen():
+    yield "cup1"
+    yield "cup2"
+    yield "cup3"
+
+
+chai = get_chai_gen()
+print(next(chai))
+print(next(chai))
+print(next(chai))
