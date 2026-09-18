7739f523f4b658179eb17e784d284adfc58314e1
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2026-09-18 14:16:15 +0530

    Add Chai class with origin and is_hot attributes

---
+++ 11_oops/simple_class.py
@@ -0,0 +1,14 @@
+class Chai:
+    origin = "India"
+
+
+print(Chai.origin)
+
+Chai.is_hot = True
+print(Chai.is_hot)
+
+# Creating objects of class
+
+masala = Chai()
+print(masala.origin)
+print(masala.is_hot)
