a476c19f473d1c05bd8cdf8e3c45a7b332ef4958
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2026-09-18 12:01:38 +0530

    Refactor chai_stall generator to handle orders and manage stall closure

--- 09_generators/close_generator.py
+++ 09_generators/close_generator.py
@@ -13,7 +13,18 @@ def full_menu():
     yield from imported_chai()
 
 
-for chai in full_menu():
-    print(chai)
+# for chai in full_menu():
+#     print(chai)
 
 
+def chai_stall():
+    try:
+        while True:
+            order = yield "Waiting for chai order"
+    except:
+        print("Stall closed no more chai")
+
+
+stall = chai_stall()
+print(next(stall))
+stall.close()
