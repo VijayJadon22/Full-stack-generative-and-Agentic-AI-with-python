23049f195ec0b6166d39d91d3659e55f3488a54a
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2026-09-18 17:04:29 +0530

    Refactor GingerChai class definition to remove duplication and clarify initialization

--- 11_oops/base_class.py
+++ 11_oops/base_class.py
@@ -3,9 +3,16 @@ class Chai:
         self.type = type_
         self.strength = strength
 
+#Duplication
+# class GingerChai(Chai):
+#     def __init__(self, type_, strength, spice_level):
+#         self.type = type_
+#         self.strength = strength
+#         self.spice_level = spice_level
 
-class GingerChai(Chai):
-    def __init__(self, type_, strength, spice_level):
-        self.type = type_
-        self.strength = strength
-        self.spice_level = spice_level
+
+#Explict call
+# class GingerChai(Chai):
+#     def __init__(self, type_, strength, spice_level):
+#         Chai.__init__(self, type_, strength)
+#         self.spice_level=spice_level
