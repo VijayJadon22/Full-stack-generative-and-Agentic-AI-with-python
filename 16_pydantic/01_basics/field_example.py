8f46f2931d52b4c83b0431a6e6d19f04669346c3
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2026-09-24 16:58:06 +0530

    Add example usage of Cart model with sample data

--- 16_pydantic/01_basics/field_example.py
+++ 16_pydantic/01_basics/field_example.py
@@ -12,3 +12,13 @@ class BlogPost(BaseModel):
     title: str
     content: str
     image_url: Optional[str] = None
+
+
+cart_data = {
+    "user_id": 123,
+    "items": ["mouse", "cpu", "printer", "keyboard"],
+    "quantities": {"mouse": 2, "cpu": 3, "printer": 4, "keyboard": 1},
+}
+
+order = Cart(**cart_data)
+print(order)
