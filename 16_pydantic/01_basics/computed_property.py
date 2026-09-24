db20e3507191035b412a6a81ee16c865354885a0
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2026-09-24 18:44:04 +0530

    Add initial implementation of Pydantic model for Booking with computed total_bill

--- 16_pydantic/01_basics/computed_property.py
+++ 16_pydantic/01_basics/computed_property.py
@@ -16,3 +16,20 @@ product_data = {"price": 12.04, "quantity": 4}
 product1 = Product(**product_data)
 print(product1.total_price)
 print(type(product1.total_price))
+
+
+class Booking(BaseModel):
+    user_id: int
+    room_id: int
+    nights: int = Field(..., ge=1)
+    rate_per_night: float
+
+    @computed_field
+    @property
+    def total_bill(self) -> float:
+        return self.nights * self.rate_per_night
+
+
+booking = Booking(user_id=1, room_id=143, nights=2, rate_per_night=120.4)
+print(booking.total_bill)
+print(booking.model_dump())
