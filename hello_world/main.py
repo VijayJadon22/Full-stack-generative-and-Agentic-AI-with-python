171b61f44dd6e9d81175b3aececaef963a356f88
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2026-09-26 22:01:45 +0530

    Add main.py for OpenAI client interaction and update .gitignore for environment files

---
+++ hello_world/main.py
@@ -0,0 +1,16 @@
+from dotenv import load_dotenv
+from openai import OpenAI
+import os
+
+load_dotenv()
+client = OpenAI(
+    api_key=os.getenv("GEMINI_API_KEY"),
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
+)
+
+response = client.chat.completions.create(
+    model="gemini-3.8-flash",
+    messages=[{"role": "user", "content": "Hey, i am Vijay Jadon! Nice to meet you"}],
+)
+
+print(response.choices[0].message.content)
