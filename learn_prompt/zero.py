fe2f6c678bab230294cbc4756786acf7253f2698
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2026-09-26 22:52:52 +0530

    Add zero.py for zero shot prompting with Gemini API integration

---
+++ learn_prompt/zero.py
@@ -0,0 +1,26 @@
+# Zero shot prompting
+from dotenv import load_dotenv
+from openai import OpenAI
+import os
+
+load_dotenv()
+
+client = OpenAI(
+    api_key=os.getenv("GEMINI_API_KEY"),
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
+)
+
+SYSTEM_PROMPT = "You should only answer coding related questions. Do not answer anything else, if someone asks anything other than coding just say Sorry. Your name is Jarvis"
+
+response = client.chat.completions.create(
+    model="gemini-3.5-flash-lite",
+    messages=[
+        {
+            "role": "system",
+            "content": SYSTEM_PROMPT,
+        },
+        {"role": "user", "content": "Explain to me how Python works in few words"},
+    ],
+)
+
+print(response.choices[0].message.content)
